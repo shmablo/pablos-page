b97a120c837549aa04dd69f7c7b7cb18ed94376a
shmablo <Pramos187@hotmail.com>
2024-11-05 16:08:07 -0500

    Added Content to the Projects Page

--- pages/projects.js
+++ pages/projects.js
@@ -7,6 +7,8 @@ import {
 import Section from "../components/section";
 import { WorkGridItem } from "../components/grid-item";
 
+import thumbDeadplant from "../public/images/deadplant.jpg";
+
 import Layout from "../components/layouts/article";
 
 const Projects = () => {
@@ -28,7 +30,23 @@ const Projects = () => {
                             Projects
                         </Heading>
                     </Center>
+
+                    <Center>Here are some of my porjects.</Center>
                 </Section>
+
+                <SimpleGrid columns={[1, 1, 2]} gap={6} paddingTop={'10px'}>
+                    <Section delay={0.5}>
+                        <WorkGridItem
+                            href="https://www.deadplant.lol/"
+                            title="Deadplant."
+                            thumbnail={thumbDeadplant}
+                        >
+                            A web application that allows users to create an account, strategize, create tier lists, and upload their strategies.
+                            Achieved top 3 First-time hacker @ Shellhacks 2024.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+
             </Container>
         </Layout>
     )
